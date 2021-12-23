6b5799273767ee38483c04836ac87afce58e65d5
Seakytin <Seakytin@yandex.ru>
2021-12-23 20:18:58 +0300

    Homework2

--- lesson-01-servlet-api/src/main/java/ru/geekbrains/ProductServlet.java
+++ lesson-01-servlet-api/src/main/java/ru/geekbrains/ProductServlet.java
@@ -8,6 +8,8 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.applet.Applet;
+import java.applet.AppletContext;
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -24,8 +26,21 @@ public class ProductServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         PrintWriter wr = resp.getWriter();
-        if (req.getParameter("id") != null) {
-            // TODO
+        String idParameter = req.getParameter("id");
+
+        if (idParameter != null) {
+            Product byId = productRepository.findById(Long.parseLong(idParameter));
+            wr.println("<table>");
+
+            wr.println("<tr>");
+            wr.println("<th>Id</th>");
+            wr.println("<th>Name</th>");
+            wr.println("</tr>");
+            wr.println("<tr>");
+            wr.println("<td>" + byId.getId() + "</td>");
+            wr.println("<td>" + byId.getName() + "</td>");
+            wr.println("</tr>");
+            wr.println("</table>");
         } else if (req.getPathInfo() == null || req.getPathInfo().equals("/")) {
             wr.println("<table>");
 
@@ -37,7 +52,8 @@ public class ProductServlet extends HttpServlet {
             for (Product product : productRepository.findAll()) {
                 wr.println("<tr>");
                 wr.println("<td>" + product.getId() + "</td>");
-                wr.println("<td>" + product.getName() + "</td>");
+                String foo = "product?id=" + product.getId();
+                wr.println("<td>  <a href = " + foo + ">"  + product.getName() + "</a> </td>");
                 // TODO <a href='product?id=12'></a>
                 wr.println("</tr>");
             }
